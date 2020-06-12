912908918e748732a57688c03dde8073c99872c5
Ryan Solida <ryansolida@gmail.com>
2020-06-12 08:57:49 -0400

    Updated CB master password

--- tools/login/admin_users.php
+++ tools/login/admin_users.php
@@ -4,6 +4,6 @@ $admin_users = array(
 	'smcbride'=>array('id'=>5002,'pw'=>'octanbearcat','priv_level'=>99),
 	'travishileman'=>array('id'=>5004,'pw'=>'cross123','priv_level'=>99),
 	'jjeren'=>array('id'=>5005,'pw'=>'jaj0722','priv_level'=>99),
-	'casselbear'=>array('id'=>5003,'pw'=>'jackson47','priv_level'=>75,'directory'=>'casselbear')
+	'casselbear'=>array('id'=>5003,'pw'=>'jackson47','priv_level'=>99)
 );
 ?>
